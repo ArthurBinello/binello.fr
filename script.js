f2b5af9a93198ac60002a080ea40b3c6127c2498
Arthur Binello <arthur.binello@gmail.com>
2017-07-25 03:21:32 +0200

    better parallax + fixes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 	$(window).scroll(function() {
 		if($(this).scrollTop() > 650 && !$('header').hasClass('fixed')) {
-		   	$('header').addClass('fixed');
-		   	$('header').slideDown(250);
+			$('header').addClass('fixed');
+			$('header').slideDown(250);
 		}
 		else if($(this).scrollTop() <= 650) {
-		   	$('header').removeClass('fixed');
-		   	$('header').slideUp(250);
+			$('header').removeClass('fixed');
+			$('header').slideUp(250);
 		}
 	});
 });
 
 function moveTo(destination) {
-	$('html, body').animate({scrollTop: $(destination).offset().top + 'px'}, 500);
+	$('html, body').animate({scrollTop: $(destination).offset().top + 'px'}, 1000);
 }
